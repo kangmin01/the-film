fe35a8c833f839ea498db1e8d870fd2a50c2159c
kangmin01 <minheek99@gmail.com>
2024-01-07 15:41:31 +0900

    initial setup

---
+++ src/app/font.ts
@@ -0,0 +1,10 @@
+import { Lobster_Two, Inter } from "next/font/google";
+
+export const lobsterTwo = Lobster_Two({
+  weight: ["400"],
+  subsets: ["latin"],
+});
+
+export const inter = Inter({
+  subsets: ["latin"],
+});
