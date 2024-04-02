aec7406618e9d3f19605d575a461bf5714c7a2f4
kangmin01 <minheek99@gmail.com>
2024-04-02 16:48:39 +0900

    fix: Update dynamic api route

--- src/app/api/discussions/route.ts
+++ src/app/api/discussions/route.ts
@@ -1,6 +1,8 @@
 import { allDiscussions } from "@/lib/discussions";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   return allDiscussions() //
     .then((data) => NextResponse.json(data));
--- src/app/api/reviews/route.ts
+++ src/app/api/reviews/route.ts
@@ -1,6 +1,8 @@
 import { allReviews } from "@/lib/reviews";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   return allReviews() //
     .then((data) => NextResponse.json(data));
